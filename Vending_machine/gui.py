0b498ab7f96fac77258aa19e27f173013727cdd2
vkvk111 <82864260+vkvk111@users.noreply.github.com>
2023-10-21 17:06:53 +0300

    Create gui.py

---
+++ Vending_machine/gui.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+
+play = b'iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAByElEQVRoge3ZMWsUQRjG8Z8RFSKCgoJp0qSJjVpoZ2clkk8g5CtYpU+TD5DSUkvbVCFNYiM2dhZqY6GFQooEISGai8Xu4HgmcnM3c+su+4fj2L2dmedhb+Z95x16enp6hljBxaZF5OAE7/GoaSGTchJ9tnCrWTnjE0zs19+HWMPlJkWNQzAyh2c4rq+/YBnnmpOWRjASuIfX0f0d3GlAVzLDRmBG9Ta+1r8d4wVuTFdaGqcZCVzFOn7Uz+ziKc5PR1oa/zISWMRm9OxbPCisK5lRjASW8Clqs4H5MrLSSTECs1jFQd3ue319KbewVFKNBBbwMmr/EY8z6kpmXCOBh3gX9dNYdjCpEbigWs326r6OVKvdlQn7TSKHkcCcKt4MNJAd5DQSuI83Ud87uJ15jL8oYYTf2cE3f2YH1wuMhXJGAtdU8+WnwtlBaSOBu3gVjZc9O5iWEapJ/wSf6zEHeI6bZzWYmY6u/4v+rzUirZ/snVh+hwPitpYFxNanKJ1IGk9L4xcz6Eom18bqg5ZtrDqx1Y2LDwPVG2lV8aH15aDWF+jOKpkWi8o5GKWIXTwq56BzxwqdOejpxNFbJw5DO3M83dPT02J+AbN50HbYDxzCAAAAAElFTkSuQmCC'
+no_titleBar = False
+fullscreen = False
+
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Button(image_data=play, key='Ok', button_color=sg.theme_background_color(), border_width=0)],
+            [sg.Button(image_data=play, key='Ok', button_color=sg.theme_background_color(), border_width=0)] ]
+
+# Create the Window
+window = sg.Window('Window Title', layout, finalize=True, no_titlebar=no_titleBar)
+window.set_min_size([800, 480])
+# Event Loop to process "events" and get the "values" of the inputs
+
+if fullscreen:
+    window.maximize()
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+        break
+
+    print(event, values)
+
+window.close()
